a7957075606bec9528495cb9c42b8fff5e55c382
TannerHollis <40923236+TannerHollis@users.noreply.github.com>
2022-02-09 19:43:53 -0500

    Memory Configuration
    Test

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -202,7 +202,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	Serial_Comm_ParseMessages();
+	Serial_Comm_CheckMessages();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
